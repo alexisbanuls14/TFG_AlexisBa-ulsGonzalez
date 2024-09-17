745346ddd4299b2dfb0f25ab05fe4f37e5c4981a
Alexis Banuls <alexisbanuls14@gmail.com>
2024-09-17 17:09:32 +0200

    1 terraform cluster despliege

---
+++ terraform/main.tf
@@ -0,0 +1,46 @@
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project_id
+  region      = var.region
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+  initial_node_count = var.min_node_count
+
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+
+  autoscaling {
+    min_node_count = var.min_node_count
+    max_node_count = var.max_node_count
+  }
+
+  logging_service   = "logging.googleapis.com/kubernetes"
+  monitoring_service = "monitoring.googleapis.com/kubernetes"
+  
+  private_cluster_config {
+    enable_private_nodes = true
+    master_ipv4_cidr_block = "10.0.0.0/28"
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+  node_count = var.min_node_count
+
+  node_config {
+    machine_type = var.machine_type
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+  }
+}
---
+++ terraform/variables.tf
@@ -0,0 +1,39 @@
+variable "project_id" {
+  description = "El ID del proyecto en Google Cloud"
+  type        = string
+}
+
+variable "region" {
+  description = "Región para desplegar el clúster"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "credentials_file" {
+  description = "Ruta al archivo de credenciales de la cuenta de servicio"
+  type        = string
+}
+
+variable "cluster_name" {
+  description = "Nombre del clúster de GKE"
+  type        = string
+  default     = "my-gke-cluster"
+}
+
+variable "machine_type" {
+  description = "Tipo de máquina para los nodos del clúster"
+  type        = string
+  default     = "e2-medium"
+}
+
+variable "min_node_count" {
+  description = "Número mínimo de nodos en el clúster"
+  type        = number
+  default     = 1
+}
+
+variable "max_node_count" {
+  description = "Número máximo de nodos en el clúster"
+  type        = number
+  default     = 5
+}
