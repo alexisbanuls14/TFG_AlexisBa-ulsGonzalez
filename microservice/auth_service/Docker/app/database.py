dfe745371986a6e01bf93db847198fd4d638f9c8
Alexis Banuls <alexisbanuls14@gmail.com>
2024-12-09 22:32:51 +0100

    FINALIZADO

--- microservice/auth_service/Docker/app/database.py
+++ microservice/auth_service/Docker/app/database.py
@@ -5,8 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 # Leer la URL de la base de datos desde las variables de entorno
-#DATABASE_URL = os.getenv("DATABASE_URL")
-DATABASE_URL = "sqlite:///./test.db"  # Para pruebas locales o variable de entorno
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
